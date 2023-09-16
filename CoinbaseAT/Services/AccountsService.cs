1c8a05eb81ed0ad689b3efee9eb6919d54ccecf7
steven confessore <confessore@balasolu.com>
2023-09-16 15:15:01 -0400

    listaccounts stringbuilder

--- CoinbaseAT/Services/AccountsService.cs
+++ CoinbaseAT/Services/AccountsService.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
 
+using System.Text;
 using CoinbaseAT.Models;
 using CoinbaseAT.Services.Abstractions;
 using CoinbaseAT.Services.Interfaces;
@@ -19,11 +20,30 @@ public class AccountsService : Service, IAccountsService
         string? cursor = null
     )
     {
-        return await SendServiceCall<List<Account>>(HttpMethod.Get, "/accounts");
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append("/v3/brokerage/accounts");
+        if (limit != null || cursor != null)
+        {
+            if (limit != null && cursor != null)
+            {
+                stringBuilder.Append($"?limit={limit}&cursor={cursor}");
+            }
+            else if (limit != null)
+            {
+                stringBuilder.Append($"?limit={limit}");
+            }
+            else if (cursor != null)
+            {
+                stringBuilder.Append($"?cursor={cursor}");
+            }
+        }
+
+        var requestPath = stringBuilder.ToString();
+        return await SendServiceCall<List<Account>>(HttpMethod.Get, requestPath);
     }
 
     public async Task<Account> GetAccountAsync(string uuid)
     {
-        return await SendServiceCall<Account>(HttpMethod.Get, $"/accounts/{uuid}");
+        return await SendServiceCall<Account>(HttpMethod.Get, $"/v3/brokerage/accounts/{uuid}");
     }
 }
