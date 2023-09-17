5c2277e6a525a8dc4461a46458568ca9d4046971
steven confessore <confessore@balasolu.com>
2023-09-17 11:40:32 -0400

    added perpetualdetails

---
+++ CoinbaseAT/Models/Interfaces/IPerpetualDetails.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+namespace CoinbaseAT.Models.Interfaces;
+
+/// <summary>
+///
+/// </summary>
+public interface IPerpetualDetails
+{
+    /// <summary>
+    ///
+    /// </summary>
+    string? Open_Interest { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    string? Funding_Rate { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    DateTime? Funding_Time { get; set; }
+}
---
+++ CoinbaseAT/Models/PerpetualDetails.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+using CoinbaseAT.Models.Interfaces;
+
+namespace CoinbaseAT.Models;
+
+/// <summary>
+/// <inheritdoc cref="IPerpetualDetails"/>
+/// </summary>
+public class PerpetualDetails : IPerpetualDetails
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Open_Interest { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Funding_Rate { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public DateTime? Funding_Time { get; set; }
+}
