5f733630a3e6b015ce08439ec28262b4b7f6429f
steven confessore <confessore@balasolu.com>
2023-09-17 12:01:12 -0400

    added fcm trading session details

---
+++ CoinbaseAT/Models/FCMTradingSessionDetails.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+using CoinbaseAT.Models.Interfaces;
+
+namespace CoinbaseAT.Models;
+
+/// <summary>
+/// <inheritdoc cref="IFCMTradingSessionDetails"/>
+/// </summary>
+public class FCMTradingSessionDetails : IFCMTradingSessionDetails
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public bool? Is_Session_Open { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public DateTime? Open_Time { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public DateTime? Close_Time { get; set; }
+}
---
+++ CoinbaseAT/Models/Interfaces/IFCMTradingSessionDetails.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+namespace CoinbaseAT.Models.Interfaces;
+
+/// <summary>
+///
+/// </summary>
+public interface IFCMTradingSessionDetails
+{
+    /// <summary>
+    ///
+    /// </summary>
+    bool? Is_Session_Open { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    DateTime? Open_Time { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    DateTime? Close_Time { get; set; }
+}
