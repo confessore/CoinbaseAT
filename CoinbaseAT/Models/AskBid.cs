92dc2290981cb679e515b28002c20a6346c214de
steven confessore <confessore@balasolu.com>
2023-09-17 12:30:45 -0400

    added ask bid

---
+++ CoinbaseAT/Models/AskBid.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+using CoinbaseAT.Models.Interfaces;
+
+namespace CoinbaseAT.Models;
+
+/// <summary>
+/// <inheritdoc cref="IAskBid"/>
+/// </summary>
+public class AskBid : IAskBid
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Price { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Size { get; set; }
+}
---
+++ CoinbaseAT/Models/Interfaces/IAskBid.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+namespace CoinbaseAT.Models.Interfaces;
+
+/// <summary>
+///
+/// </summary>
+public interface IAskBid
+{
+    /// <summary>
+    ///
+    /// </summary>
+    string? Price { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    string? Size { get; set; }
+}
