d1481725485ea2e8f7c69fc1da58509908a5fee1
steven confessore <confessore@balasolu.com>
2023-09-17 14:07:27 -0400

    added limit limit gtd

---
+++ CoinbaseAT/Models/Interfaces/ILimitLimitGTD.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+namespace CoinbaseAT.Models.Interfaces;
+
+/// <summary>
+/// <inheritdoc cref="ILimitLimitGTD"/>
+/// </summary>
+public interface ILimitLimitGTD
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    string? Base_Size { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    string? Limit_Price { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    DateTime? End_Time { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    bool? Post_Only { get; set; }
+}
---
+++ CoinbaseAT/Models/LimitLimitGTD.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+using CoinbaseAT.Models.Interfaces;
+
+namespace CoinbaseAT.Models;
+
+/// <summary>
+/// <inheritdoc cref="ILimitLimitGTD"/>
+/// </summary>
+public class LimitLimitGTD : ILimitLimitGTD
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Base_Size { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Limit_Price { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public DateTime? End_Time { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public bool? Post_Only { get; set; }
+}
