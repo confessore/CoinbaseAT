d80a7c25d36809318015b10926b616acc754a41a
steven confessore <confessore@balasolu.com>
2023-09-17 11:59:02 -0400

    added future product details

---
+++ CoinbaseAT/Models/FutureProductDetails.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+using CoinbaseAT.Models.Interfaces;
+
+namespace CoinbaseAT.Models;
+
+/// <summary>
+/// <inheritdoc cref="IFutureProductDetails"/>
+/// </summary>
+public class FutureProductDetails : IFutureProductDetails
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Venue { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Contract_Code { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public DateTime? Contract_Expiry { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Contract_Size { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Contract_Root_Unit { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Group_Description { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Contract_Expiry_Timezone { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Group_Short_Description { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Risk_Managed_By { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Contract_Expiry_Type { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public PerpetualDetails? Perpetual_Details { get; set; }
+}
---
+++ CoinbaseAT/Models/Interfaces/IFutureProductDetails.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+namespace CoinbaseAT.Models.Interfaces;
+
+/// <summary>
+///
+/// </summary>
+public interface IFutureProductDetails
+{
+    /// <summary>
+    ///
+    /// </summary>
+    string? Venue { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    string? Contract_Code { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    DateTime? Contract_Expiry { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    string? Contract_Size { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    string? Contract_Root_Unit { get; set; }
+
+    /// <summary>
+    /// Descriptive name for the product series, eg "Nano Bitcoin Futures".
+    /// </summary>
+    string? Group_Description { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    string? Contract_Expiry_Timezone { get; set; }
+
+    /// <summary>
+    /// Short version of the group_description, eg "Nano BTC".
+    /// </summary>
+    string? Group_Short_Description { get; set; }
+
+    /// <summary>
+    /// Possible values: [UNKNOWN_RISK_MANAGEMENT_TYPE, MANAGED_BY_FCM, MANAGED_BY_VENUE]
+    /// </summary>
+    string? Risk_Managed_By { get; set; }
+
+    /// <summary>
+    /// Possible values: [UNKNOWN_CONTRACT_EXPIRY_TYPE, EXPIRING]
+    /// </summary>
+    string? Contract_Expiry_Type { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
+    PerpetualDetails? Perpetual_Details { get; set; }
+}
