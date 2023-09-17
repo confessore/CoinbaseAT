c6b62e1f06dcbbae5e9366c6437e1f73c34cc04e
steven confessore <confessore@balasolu.com>
2023-09-17 12:18:04 -0400

    added product list and edited product access mods

---
+++ CoinbaseAT/Models/Interfaces/IProductList.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+namespace CoinbaseAT.Models.Interfaces;
+
+/// <summary>
+///
+/// </summary>
+public interface IProductList
+{
+    /// <summary>
+    /// Array of objects, each representing one product.
+    /// </summary>
+    IEnumerable<Product>? Products { get; set; }
+
+    /// <summary>
+    /// Number of products that were returned.
+    /// </summary>
+    int? Num_Products { get; set; }
+}
---
+++ CoinbaseAT/Models/ProductList.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+using CoinbaseAT.Models.Interfaces;
+
+namespace CoinbaseAT.Models;
+
+/// <summary>
+/// <inheritdoc cref="IProductList"/>
+/// </summary>
+public class ProductList : IProductList
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public IEnumerable<Product>? Products { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public int? Num_Products { get; set; }
+}
