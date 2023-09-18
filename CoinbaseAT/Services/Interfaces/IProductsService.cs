a7f16cf713afc998119908af5c30b444760c03a4
steven confessore <confessore@balasolu.com>
2023-09-18 14:15:28 -0400

    added get best bid ask

--- CoinbaseAT/Services/Interfaces/IProductsService.cs
+++ CoinbaseAT/Services/Interfaces/IProductsService.cs
@@ -10,6 +10,22 @@ namespace CoinbaseAT.Services.Interfaces;
 /// </summary>
 public interface IProductsService
 {
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="product_ids"></param>
+    /// <returns></returns>
+    Task<PricebooksResponse> GetBestBidAskAsync(string[]? product_ids = null);
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="limit"></param>
+    /// <param name="offset"></param>
+    /// <param name="product_type"></param>
+    /// <param name="product_ids"></param>
+    /// <param name="contract_expiry_type"></param>
+    /// <returns></returns>
     Task<ProductsResponse> ListProductsAsync(
         int limit = 0,
         int? offset = null,
@@ -18,5 +34,10 @@ public interface IProductsService
         string? contract_expiry_type = null
     );
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="product_id"></param>
+    /// <returns></returns>
     Task<Product> GetProductAsync(string product_id);
 }
--- CoinbaseAT/Services/ProductsService.cs
+++ CoinbaseAT/Services/ProductsService.cs
@@ -17,6 +17,38 @@ public class ProductsService : CoinbaseATService, IProductsService
     public ProductsService(IHttpClientService httpClientService)
         : base(httpClientService) { }
 
+    public async Task<PricebooksResponse> GetBestBidAskAsync(string[]? product_ids = null)
+    {
+        var requestPath = "/api/v3/brokerage/best_bid_ask";
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append(requestPath);
+
+        if (product_ids != null)
+        {
+            for (var i = 0; i < product_ids.Length; i++)
+            {
+                if (i != 0)
+                {
+                    stringBuilder.Append($"&product_ids={product_ids[i]}");
+                    continue;
+                }
+
+                if (i == 0)
+                {
+                    stringBuilder.Append($"?product_ids={product_ids[i]}");
+                }
+            }
+        }
+
+        var fullRequestPath = stringBuilder.ToString();
+        return await SendServiceCall<PricebooksResponse>(
+            HttpMethod.Get,
+            requestPath,
+            fullRequestPath,
+            string.Empty
+        );
+    }
+
     public async Task<ProductsResponse> ListProductsAsync(
         int limit = 0,
         int? offset = null,
@@ -31,25 +63,25 @@ public class ProductsService : CoinbaseATService, IProductsService
         stringBuilder.Append($"?limit={limit}");
         if (offset != null)
         {
-            stringBuilder.Append($"?offset={offset}");
+            stringBuilder.Append($"&offset={offset}");
         }
 
         if (product_type != null)
         {
-            stringBuilder.Append($"?product_type={product_type}");
+            stringBuilder.Append($"&product_type={product_type}");
         }
 
         if (product_ids != null)
         {
             foreach (var product_id in product_ids)
             {
-                stringBuilder.Append($"?product_ids={product_id}");
+                stringBuilder.Append($"&product_ids={product_id}");
             }
         }
 
         if (contract_expiry_type != null)
         {
-            stringBuilder.Append($"?contract_expiry_type={contract_expiry_type}");
+            stringBuilder.Append($"&contract_expiry_type={contract_expiry_type}");
         }
 
         var fullRequestPath = stringBuilder.ToString();
