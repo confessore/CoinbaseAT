a624e71e992cb3b49c164847b0e05f49e2fdb9a3
steven confessore <confessore@balasolu.com>
2023-09-18 10:49:38 -0400

    added order list

---
+++ CoinbaseAT/Models/OrderList.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using CoinbaseAT.Models.Interfaces;
+
+namespace CoinbaseAT.Models;
+
+/// <summary>
+/// <inheritdoc cref="IOrderList"/>
+/// </summary>
+public class OrderList : IOrderList
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public IEnumerable<Order>? Orders { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public long? Sequence { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public bool? Has_Next { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Cursor { get; set; }
+}
