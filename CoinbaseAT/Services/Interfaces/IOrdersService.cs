bf0158966a80093cdd609a9a6cb7a4bf8adac62d
steven confessore <confessore@balasolu.com>
2023-09-18 16:01:54 -0400

    added list orders

--- CoinbaseAT/Services/Interfaces/IOrdersService.cs
+++ CoinbaseAT/Services/Interfaces/IOrdersService.cs
@@ -22,4 +22,54 @@ public interface IOrdersService
         string? client_order_id = null,
         string? user_native_currency = null
     );
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="order_id"></param>
+    /// <param name="product_id"></param>
+    /// <param name="start_sequence_timestamp"></param>
+    /// <param name="end_sequence_timestamp"></param>
+    /// <param name="limit"></param>
+    /// <param name="cursor"></param>
+    /// <returns></returns>
+    Task<FillsResponse> ListFillsAsync(
+        string? order_id = null,
+        string? product_id = null,
+        string? start_sequence_timestamp = null,
+        string? end_sequence_timestamp = null,
+        long? limit = null,
+        string? cursor = null
+    );
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="product_id"></param>
+    /// <param name="order_status"></param>
+    /// <param name="limit"></param>
+    /// <param name="start_date"></param>
+    /// <param name="end_date"></param>
+    /// <param name="user_native_currency"></param>
+    /// <param name="order_type"></param>
+    /// <param name="order_side"></param>
+    /// <param name="cursor"></param>
+    /// <param name="product_type"></param>
+    /// <param name="order_placement_source"></param>
+    /// <param name="contract_expiry_type"></param>
+    /// <returns></returns>
+    Task<FillsResponse> ListOrdersAsync(
+        string? product_id = null,
+        string[]? order_status = null,
+        int? limit = null,
+        string? start_date = null,
+        string? end_date = null,
+        string? user_native_currency = null,
+        string? order_type = null,
+        string? order_side = null,
+        string? cursor = null,
+        string? product_type = null,
+        string? order_placement_source = null,
+        string? contract_expiry_type = null
+    );
 }
--- CoinbaseAT/Services/OrdersService.cs
+++ CoinbaseAT/Services/OrdersService.cs
@@ -53,4 +53,163 @@ public class OrdersService : CoinbaseATService, IOrdersService
             string.Empty
         );
     }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public async Task<FillsResponse> ListFillsAsync(
+        string? order_id = null,
+        string? product_id = null,
+        string? start_sequence_timestamp = null,
+        string? end_sequence_timestamp = null,
+        long? limit = null,
+        string? cursor = null
+    )
+    {
+        var requestPath = $"/api/v3/brokerage/orders/historical/fill";
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append(requestPath);
+        if (order_id != null)
+        {
+            stringBuilder.Append($"?order_id={order_id}");
+        }
+
+        if (product_id != null)
+        {
+            stringBuilder.Append($"&product_id={product_id}");
+        }
+
+        if (start_sequence_timestamp != null)
+        {
+            stringBuilder.Append($"&start_sequence_timestamp={start_sequence_timestamp}");
+        }
+
+        if (end_sequence_timestamp != null)
+        {
+            stringBuilder.Append($"&end_sequence_timestamp={end_sequence_timestamp}");
+        }
+
+        if (limit != null)
+        {
+            stringBuilder.Append($"&limit={limit}");
+        }
+
+        if (cursor != null)
+        {
+            stringBuilder.Append($"&cursor={cursor}");
+        }
+
+        var fullRequestPath = stringBuilder.ToString();
+        if (fullRequestPath.Contains('&') && !fullRequestPath.Contains('?'))
+        {
+            fullRequestPath = fullRequestPath.Remove(requestPath.Length, 1);
+            fullRequestPath = fullRequestPath.Insert(requestPath.Length, "?");
+        }
+
+        return await SendServiceCall<FillsResponse>(
+            HttpMethod.Get,
+            requestPath,
+            fullRequestPath,
+            string.Empty
+        );
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public async Task<FillsResponse> ListOrdersAsync(
+        string? product_id = null,
+        string[]? order_status = null,
+        int? limit = null,
+        string? start_date = null,
+        string? end_date = null,
+        string? user_native_currency = null,
+        string? order_type = null,
+        string? order_side = null,
+        string? cursor = null,
+        string? product_type = null,
+        string? order_placement_source = null,
+        string? contract_expiry_type = null
+    )
+    {
+        var requestPath = $"/api/v3/brokerage/orders/historical/batch";
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append(requestPath);
+        if (product_id != null)
+        {
+            stringBuilder.Append($"?product_id={product_id}");
+        }
+
+        if (order_status != null)
+        {
+            foreach (var status in order_status)
+            {
+                stringBuilder.Append($"&order_status={status}");
+            }
+        }
+
+        if (limit != null)
+        {
+            stringBuilder.Append($"&limit={limit}");
+        }
+
+        if (start_date != null)
+        {
+            stringBuilder.Append($"&start_date={start_date}");
+        }
+
+        if (end_date != null)
+        {
+            stringBuilder.Append($"&end_date={end_date}");
+        }
+
+        if (user_native_currency != null)
+        {
+            stringBuilder.Append($"&user_native_currency={user_native_currency}");
+        }
+
+        if (order_type != null)
+        {
+            stringBuilder.Append($"&order_type={order_type}");
+        }
+
+        if (order_side != null)
+        {
+            stringBuilder.Append($"&order_side={order_side}");
+        }
+
+        if (cursor != null)
+        {
+            stringBuilder.Append($"&cursor={cursor}");
+        }
+
+        if (product_type != null)
+        {
+            stringBuilder.Append($"&product_type={product_type}");
+        }
+
+        if (order_placement_source != null)
+        {
+            stringBuilder.Append($"&order_placement_source={order_placement_source}");
+        }
+
+        if (contract_expiry_type != null)
+        {
+            stringBuilder.Append($"&contract_expiry_type={contract_expiry_type}");
+        }
+
+        var fullRequestPath = stringBuilder.ToString();
+        if (fullRequestPath.Contains('&') && !fullRequestPath.Contains('?'))
+        {
+            fullRequestPath = fullRequestPath.Remove(requestPath.Length, 1);
+            fullRequestPath = fullRequestPath.Insert(requestPath.Length, "?");
+        }
+
+        return await SendServiceCall<FillsResponse>(
+            HttpMethod.Get,
+            requestPath,
+            fullRequestPath,
+            string.Empty
+        );
+    }
 }
