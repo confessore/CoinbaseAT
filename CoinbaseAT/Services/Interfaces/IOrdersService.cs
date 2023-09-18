c7c10629a802b47e9762215fc57599abaa37317c
steven confessore <confessore@balasolu.com>
2023-09-18 15:19:16 -0400

    added get order

--- CoinbaseAT/Services/Interfaces/IOrdersService.cs
+++ CoinbaseAT/Services/Interfaces/IOrdersService.cs
@@ -1,8 +1,25 @@
 ﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
 
+using System.Threading.Tasks;
+using CoinbaseAT.Models;
+
 namespace CoinbaseAT.Services.Interfaces;
 
 /// <summary>
 ///
 /// </summary>
-public interface IOrdersService { }
+public interface IOrdersService
+{
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="order_id"></param>
+    /// <param name="client_order_id"></param>
+    /// <param name="user_native_currency"></param>
+    /// <returns></returns>
+    Task<OrderResponse> GetOrderAsync(
+        string order_id,
+        string? client_order_id = null,
+        string? user_native_currency = null
+    );
+}
--- CoinbaseAT/Services/OrdersService.cs
+++ CoinbaseAT/Services/OrdersService.cs
@@ -1,5 +1,9 @@
 ﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
 
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CoinbaseAT.Models;
 using CoinbaseAT.Services.Abstractions;
 using CoinbaseAT.Services.Interfaces;
 
@@ -12,4 +16,41 @@ public class OrdersService : CoinbaseATService, IOrdersService
 {
     public OrdersService(IHttpClientService httpClientService)
         : base(httpClientService) { }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public async Task<OrderResponse> GetOrderAsync(
+        string order_id,
+        string? client_order_id = null,
+        string? user_native_currency = null
+    )
+    {
+        var requestPath = $"/api/v3/brokerage/orders/historical/{order_id}";
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append(requestPath);
+        if (client_order_id != null)
+        {
+            stringBuilder.Append($"?client_order_id={client_order_id}");
+        }
+
+        if (user_native_currency != null)
+        {
+            stringBuilder.Append($"&user_native_currency={user_native_currency}");
+        }
+
+        var fullRequestPath = stringBuilder.ToString();
+        if (fullRequestPath.Contains('&') && !fullRequestPath.Contains('?'))
+        {
+            fullRequestPath = fullRequestPath.Remove(requestPath.Length, 1);
+            fullRequestPath = fullRequestPath.Insert(requestPath.Length, "?");
+        }
+
+        return await SendServiceCall<OrderResponse>(
+            HttpMethod.Get,
+            requestPath,
+            fullRequestPath,
+            string.Empty
+        );
+    }
 }
