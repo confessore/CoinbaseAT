0a124c61d71cdec799c2f087665ecb35bb4d4427
steven confessore <confessore@balasolu.com>
2023-09-18 11:03:21 -0400

    added result list

---
+++ CoinbaseAT/Models/Interfaces/IResultList.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace CoinbaseAT.Models.Interfaces;
+
+/// <summary>
+///
+/// </summary>
+public interface IResultList
+{
+    /// <summary>
+    /// The result of initiated cancel requests
+    /// </summary>
+    public IEnumerable<Result>? Results { get; set; }
+
+    /// <summary>
+    /// Whether the cancel request was submitted successfully.
+    /// </summary>
+    public string? Success { get; set; }
+
+    /// <summary>
+    /// Possible values: [UNKNOWN_CANCEL_FAILURE_REASON, INVALID_CANCEL_REQUEST, UNKNOWN_CANCEL_ORDER, COMMANDER_REJECTED_CANCEL_ORDER, DUPLICATE_CANCEL_REQUEST]
+    /// </summary>
+    public string? Failure_Reason { get; set; }
+
+    /// <summary>
+    /// The IDs of order cancel request was initiated for
+    /// </summary>
+    public string? Order_Id { get; set; }
+}
---
+++ CoinbaseAT/Models/ResultList.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Steven Confessore - Balanced Solutions Software - CoinbaseAT Contributors.  All Rights Reserved.  Licensed under the MIT license.  See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using CoinbaseAT.Models.Interfaces;
+
+namespace CoinbaseAT.Models;
+
+/// <summary>
+/// <inheritdoc cref="IResultList"/>
+/// </summary>
+public class ResultList : IResultList
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public IEnumerable<Result>? Results { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Success { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Failure_Reason { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string? Order_Id { get; set; }
+}
