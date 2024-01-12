1dd4b976c892311b3e86efdc115e93c317fe8061
steven confessore <confessore@balasolu.com>
2024-01-12 11:40:26 -0500

    more standard declarations

--- CoinbaseAT/Models/Account.cs
+++ CoinbaseAT/Models/Account.cs
@@ -3,13 +3,14 @@
 using System;
 using CoinbaseAT.Models.Interfaces;
 
-namespace CoinbaseAT.Models;
-
-/// <summary>
-/// <inheritdoc cref="IAccount"/>
-/// </summary>
-public class Account : IAccount
+namespace CoinbaseAT.Models
 {
+
+    /// <summary>
+    /// <inheritdoc cref="IAccount"/>
+    /// </summary>
+    public class Account : IAccount
+    {
 #if NET7_0_OR_GREATER
     /// <summary>
     /// <inheritdoc/>
@@ -71,64 +72,65 @@ public class Account : IAccount
     /// </summary>
     public AccountHold? Hold { get; set; }
 #elif NETSTANDARD2_0_OR_GREATER
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Uuid { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Uuid { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string ProfileId { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string ProfileId { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Currency { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Currency { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public AccountBalance Available_Balance { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public AccountBalance Available_Balance { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public bool Default { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool Default { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public bool Active { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool Active { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public DateTime Created_At { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public DateTime Created_At { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public DateTime Updated_At { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public DateTime Updated_At { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public DateTime Deleted_At { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public DateTime Deleted_At { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Type { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Type { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public bool Ready { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool Ready { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public AccountHold Hold { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public AccountHold Hold { get; set; }
 #endif
+    }
 }
--- CoinbaseAT/Models/Order.cs
+++ CoinbaseAT/Models/Order.cs
@@ -3,13 +3,13 @@
 using System;
 using CoinbaseAT.Models.Interfaces;
 
-namespace CoinbaseAT.Models;
-
-/// <summary>
-/// <inheritdoc cref="IOrder"/>
-/// </summary>
-public class Order : IOrder
+namespace CoinbaseAT.Models
 {
+    /// <summary>
+    /// <inheritdoc cref="IOrder"/>
+    /// </summary>
+    public class Order : IOrder
+    {
 #if NET7_0_OR_GREATER
     /// <summary>
     /// <inheritdoc/>
@@ -161,154 +161,155 @@ public class Order : IOrder
     /// </summary>
     public bool? Is_Liquidation { get; set; }
 #elif NETSTANDARD2_0_OR_GREATER
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Order_Id { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Order_Id { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Product_Id { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Product_Id { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string User_Id { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string User_Id { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public OrderConfiguration Order_Configuration { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public OrderConfiguration Order_Configuration { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Side { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Side { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Client_Order_Id { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Client_Order_Id { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Status { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Status { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Time_In_Force { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Time_In_Force { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public DateTime Created_Time { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public DateTime Created_Time { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Completion_Percentage { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Completion_Percentage { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Filled_Size { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Filled_Size { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Average_Filled_Price { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Average_Filled_Price { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Fee { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Fee { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Number_Of_Fills { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Number_Of_Fills { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Filled_Value { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Filled_Value { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public bool Pending_Cancel { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool Pending_Cancel { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public bool Size_In_Quote { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool Size_In_Quote { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Total_Fees { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Total_Fees { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public bool Size_Inclusive_Of_Fees { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool Size_Inclusive_Of_Fees { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Total_Value_After_Fees { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Total_Value_After_Fees { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Trigger_Status { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Trigger_Status { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Order_Type { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Order_Type { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Reject_Reason { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Reject_Reason { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public bool Settled { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool Settled { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Product_Type { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Product_Type { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Reject_Message { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Reject_Message { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Cancel_Message { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Cancel_Message { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Order_Placement_Source { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Order_Placement_Source { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public string Outstanding_Hold_Amount { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public string Outstanding_Hold_Amount { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public bool Is_Liquidation { get; set; }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool Is_Liquidation { get; set; }
 #endif
+    }
 }
